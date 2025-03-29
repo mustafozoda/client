c26438978ace18a86a390ee830d54cf1c6457a60
Kristof924 <B.Kristof2001@gmail.com>
2025-03-29 18:12:31 +0100

    API_BASE_URL mod

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_BASE_API_URL.replace(/\/$/, "");
 
 const getToken = () => sessionStorage.getItem("authToken") || localStorage.getItem("authToken");
 
