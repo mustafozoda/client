a3c1407a27c4fda3fb4ad6a5d1428928419019b7
mustafozoda <mustafozodashq888@gmail.com>
2025-05-23 01:21:48 +0200

    chore: update dependencies and modify articles
    - Updated React and React DOM to version 19.1.0 in package.json.
    - Updated TypeScript types for React and React DOM to version 19.1.0.
    - Changed article source from CNET to Umich.edu with new article details in articles.json.
    - Commented out components and keyframes in index.css for potential future use.
    - Rearranged imports in main.jsx for better organization.
    - Removed index.css from the content array in tailwind.config.js.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
 import "./i18n";
+import "./index.css";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
